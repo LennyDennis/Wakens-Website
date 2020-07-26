5d6f3cdc1236d738b0224f08bce5691589df0d14
LennyDennis <lennydennis@gmail.com>
2020-07-26 17:52:08 +0300

    Refactor the error and success page

--- emailsuccess.php
+++ emailsuccess.php
@@ -1,83 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <title>Contact | Hanawell Limited</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="hanawell, hanawell limited, sap, sap training, sap consultation" name="keywords">
-  <meta content="this the sap implementation page for hanawell limited" name="description">
-
-  <!-- Favicons -->
- <link href="images/favIcon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
-
-  <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Libraries CSS Files -->
-  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="lib/animate/animate.min.css" rel="stylesheet">
-  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet">
-  <link href="css/contact.css" rel="stylesheet">
-  <link href="css/consultation.css" rel="stylesheet">
+    <title>Contact | Hanawell Limited</title>
+    <?php include_once("header.php") ?>
+    <link href="css/consultation.css" rel="stylesheet">
 </head>
 
 <body>
-
-
-<?php include_once("navigationbar.php")?>
-
-
-  <main id="main">
-
-    <section id="contact" class="section-bg fadeInUp">
-      <div class="container email-error">
-
-        <div class="thankyoumain text-center">
-            <div class="message">
-                <h3 class="text-success">THANK YOU!</h3>
-                <p>We are reviewing your submission and will get back to you soon!</p>
+    <?php include_once("navigationbar.php")?>
+    <main id="main">
+        <section id="contact" class="section-bg fadeInUp">
+            <div class="container email-error">
+                <div class="thankyoumain text-center">
+                    <div class="message">
+                        <h3 class="text-success">THANK YOU!</h3>
+                        <p>We are reviewing your submission and will get back to you soon!</p>
+                    </div>
+                    <div class="homepage">
+                        <p>Go back to homepage</p>
+                        <a href="index.php" class="btn-get-started btn-reachus">Home Page</a>
+                    </div>
+                </div>
             </div>
-            <div class="homepage">
-                <p>Go back to homepage</p>
-                <a href="index.php" class="btn-get-started btn-reachus">Home Page</a>
-            </div>
-        </div>
-
-      </div>
-    </section>
-
-  </main>
-
-  <?php include_once("footer.php")?>
-
-  <!-- JavaScript Libraries -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/jquery/jquery-migrate.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="lib/easing/easing.min.js"></script>
-  <script src="lib/superfish/hoverIntent.js"></script>
-  <script src="lib/superfish/superfish.min.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/waypoints/waypoints.min.js"></script>
-  <script src="lib/counterup/counterup.min.js"></script>
-  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="lib/isotope/isotope.pkgd.min.js"></script>
-  <script src="lib/lightbox/js/lightbox.min.js"></script>
-  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
-  <!-- Contact Form JavaScript File -->
-  <script src="contactform/contactform.js"></script>
-
-  <!-- Template Main Javascript File -->
-  <script src="js/main.js"></script>
-
+        </section>
+    </main>
+    <?php include_once("footer.php")?>
 </body>
+
 </html>
--- sendmail.php
+++ sendmail.php
@@ -1,39 +1,38 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-        $to = 'lennydennis@gmail.com';
-        //get fields from the form
-        $from = filter_var(trim($_POST['email']),FILTER_SANITIZE_EMAIL);
-        $fullname = strip_tags(trim($_POST['fullname']));
-        $fullname = str_replace(array("\r","\n"),array(" "," "),$fullname);
-        $phone = trim($_POST['phone']);
-        $company = trim($_POST['company']);
-        $subject = trim($_POST['subject']);
-        $message = trim($_POST['message']);
+$to = 'lennydennis@gmail.com';
+//get fields from the form
+$from = $_POST['email'];
 
-        $headers = "From: ".$from."\r\n" ;
-        $headers = "Reply-To: ".$to."\r\n" ;
-        $headers.= "MIME-Version: 1.0\r\n";
-        $headers.= "Content-Type: text/html\r\n";
+// $from = filter_var(trim($_POST['email']),FILTER_SANITIZE_EMAIL);
+$fullname = strip_tags(trim($_POST['fullname']));
+$fullname = str_replace(array("\r","\n"),array(" "," "),$fullname);
+$phone = trim($_POST['phone']);
+$company = trim($_POST['company']);
+$subject = trim($_POST['subject']);
+$message = trim($_POST['message']);
 
-        $fullmessage =  'Name: '.$fullname."\r\n"
-                        .'Email: '.$from."\r\n"
-                        .'Phone: '.$phone."\r\n"
-                        .'Company: '.$company."\r\n\n"
-                        .'Subject: '.$subject."\r\n"
-                        .'Email Body: '."\r\n".$message;
+$headers = "From: ".$from."\r\n" ;
+// $headers = "Reply-To: ".$to."\r\n" ;
+// $headers.= "MIME-Version: 1.0\r\n";
+// $headers.= "Content-Type: text/html\r\n";
 
-        $result = mail($to,$subject,$fullmessage,$headers);
-        if($result) {  
-            http_response_code(200);
-            header('Location: emailsuccess.php');
-        } else {
-            http_response_code(500);
-            header('Location: emailerror.php');
-        }
+$fullmessage = 'Name: '.$fullname."\r\n"
+.'Email: '.$from."\r\n"
+.'Phone: '.$phone."\r\n"
+.'Company: '.$company."\r\n\n"
+.'Subject: '.$subject."\r\n"
+.'Email Body: '."\r\n".$message;
 
-    }
+$result = mail($to,$subject,$fullmessage,$headers);
+if($result) {
+header('Location: emailsuccess.php');
+} else {
+http_response_code(500);
+header('Location: emailerror.php');
+}
+
+}
 ?>
